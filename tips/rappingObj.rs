134cc47d3a0c6e8dcf5395ee8eafb7d11ed183c0
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-04-04 14:22:27 +0900

    my tips

---
+++ tips/rappingObj.rs
@@ -0,0 +1,13 @@
+use core::{fmt::Error, error};
+
+fn main() {
+    let new_list= pass_values();
+}
+
+pub async fn pass_values() -> Result<Vec<usize>, ()>{
+    let mut list: Vec<usize>= Vec::new();
+    list.push(1);
+    list.push(2);
+
+    return Ok(list);
+}
---
+++ tips/usageFilterMap.rs
@@ -0,0 +1,9 @@
+    //let mut paths: Vec<PathBuf>= fs::read_dir(SQL_DIR)?
+    //    .into_iter()
+    //    .filter_map(|e| e.ok.map(|e| e.path()))
+    //    .collect();
+    //
+    //
+fn main(){
+    
+}
