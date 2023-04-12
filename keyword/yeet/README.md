c1d0e1f8ee8521270e28a50e3528d263041698a6
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-04-12 13:18:05 +0900

    yeet

---
+++ keyword/yeet/README.md
@@ -0,0 +1,7 @@
+# Yeet macro
+
+https://www.youtube.com/watch?v=BgCXrf_SG2E  
+
+https://doc.rust-lang.org/std/ops/struct.Yeet.html  
+
+
---
+++ keyword/yeet/yeet_ex.rs
@@ -0,0 +1,8 @@
+#![feature(yeet_expr)]
+
+fn divide(dividend: i32, divisor: i32) -> Option<i32>{
+    if divisor == 0 {
+        do yeet
+    }
+
+    return Some(dividend / divisor);
